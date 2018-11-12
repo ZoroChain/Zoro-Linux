6ff3c97cf3c13fc6177136d4ffa6aba14e0fe951
ProDog <xiuboard@foxmail.com>
2018-11-12 16:33:27 +0800

    update

--- dellib.sh
+++ dellib.sh
@@ -1,1 +1,1 @@
-rm -rf Chain_01842FFB Index_01842FFB Logs AppChain;echo "delete success";mkdir AppChain;echo "create AppChain"
+rm -rf Chain_01842FFB Index_01842FFB Logs AppChain;echo "delete success"
